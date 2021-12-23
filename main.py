c0eb9f306a06e5fcb18d134e5c18f5e641405c85
killthebee <superhotfire447@yandex.ru>
2021-12-23 10:00:12 +0300

    add send message script

--- main.py
+++ main.py
@@ -5,11 +5,26 @@ import argparse
 
 
 async def chat_client(host, port, path):
-    reader, writer = await asyncio.open_connection(host, 5000)
+    #4652777a-6261-11ec-8c47-0242ac110002
+    reader, writer = await asyncio.open_connection(host, port)
     while True:
         data = await reader.read(100)
-        async with aiofiles.open(path, "a") as f:
+        async with aiofiles.open(path, 'a') as f:
             await f.write(f"[{datetime.datetime.now().strftime('%d.%m.%y %H:%M')}] {data.decode()}")
+
+
+async def send_message(host, port, path):
+    reader, writer = await asyncio.open_connection('minechat.dvmn.org', 5050)
+    writer.write('4652777a-6261-11ec-8c47-0242ac110002\n\n'.encode())
+
+    await writer.drain()
+
+    writer.write('33\n\n'.encode())
+    await writer.drain()
+
+    print('2')
+
+    writer.close()
 
 
 if __name__ == '__main__':
@@ -19,4 +34,5 @@ if __name__ == '__main__':
     parser.add_argument('--history', default='chat.txt', help='path to txt file')
     args = parser.parse_args()
 
-    asyncio.run(chat_client(args.host, args.port, args.history))
+    asyncio.run(send_message(args.host, args.port, args.history))
+    # asyncio.run(chat_client(args.host, args.port, args.history))
